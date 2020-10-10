695feac8f4688f3be42d3d17ca0e5fda29828c1f
Nio Wang <swan4287@uni.sydney.edu.au>
2020-10-10 18:14:35 +1100

    created utils folder and added a temp searchbar file

---
+++ src/components/utils/SearchBar.vue
@@ -0,0 +1,14 @@
+<!-- Search bar component -->
+<template>
+    <h1>Search bar component temp file</h1>
+</template>
+
+<script>
+    export default {
+        name: "SearchBar.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
