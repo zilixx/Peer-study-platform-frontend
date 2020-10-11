198f46f9ce76c37a492f9cfd53030bbf41dcf5b2
Qihang Zhao <qzha5529@uni.sydney.edu.au>
2020-10-11 21:20:38 +1100

    register front-end fage

--- src/components/content/Resigter.vue
+++ src/components/content/Resigter.vue
@@ -1,13 +1,55 @@
 <template>
-  <h1>Resigter as tutor here</h1>
+  <div>
+    <h3>Select Courses</h3>
+    <el-select v-model="value" placeholder="please choose" id="selection" class="selection">
+      <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+      </el-option>
+    </el-select>
+
+    <uploading></uploading>
+  </div>
+
 </template>
 
 <script>
+import uploading from "../utils/Upload"
+
 export default {
-    name: "Register"
+  name: "Register",
+  components: {
+    uploading
+  },
+  data() {
+    return {
+      options: [{
+        value: 'Option1',
+        label: 'ELEC5619'
+      }, {
+        value: 'Option2',
+        label: 'ELEC5620'
+      }, {
+        value: 'Option3',
+        label: 'COMP5347'
+      }, {
+        value: 'Option4',
+        label: 'INFO5990'
+      }, {
+        value: 'Option5',
+        label: 'INFO6007'
+      }],
+      value: ''
+    }
+  }
 }
 </script>
 
 <style>
-
+.selection{
+  width: 400px;
+  margin: 20px auto;
+}
 </style>
---
+++ src/components/utils/Upload.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h3>Upload CV</h3>
+    <el-upload
+        class="upload"
+        ref="upload"
+        action="https://jsonplaceholder.typicode.com/posts/"
+        :on-preview="handlePreview"
+        :on-remove="handleRemove"
+        :before-remove="beforeRemove"
+        :limit="1"
+        :file-list="fileList"
+        :auto-upload="false">
+      <el-button slot="trigger" size="small" type="primary">Select a file</el-button>
+      <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">Confirm</el-button>
+      <div slot="tip" class="el-upload__tip">No larger than 10MB</div>
+    </el-upload>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Upload",
+  data() {
+    return {
+      fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
+    };
+  },
+  methods: {
+    submitUpload() {
+      this.$refs.upload.submit();
+    },
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePreview(file) {
+      console.log(file);
+    },
+    beforeRemove(file) {
+      return this.$confirm(`Are you sure to delete ${ file.name }？`);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.upload {
+  width: 400px;
+  margin: 20px auto;
+}
+</style>
