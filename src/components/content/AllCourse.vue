11a00844c2dd3db5ae191f67e18bfa34676bd1f9
Nio Wang <swan4287@uni.sydney.edu.au>
2020-10-11 18:57:55 +1100

    changed the searchbar to a simple input bar with a search button

--- src/components/content/AllCourse.vue
+++ src/components/content/AllCourse.vue
@@ -1,10 +1,29 @@
 <template>
-  <h1>This is course overview content</h1>
+  <div>
+    <h1>This is course overview content</h1>
+    Test:<Search_input v-model="inputvalue"></Search_input>
+    <button @click="submit">Search</button>
+  </div>
+
 </template>
 
 <script>
-export default {
-    name: "AllCourse"
+  import SearchBar from "../utils/SearchBar";
+  export default {
+    name: "AllCourse",
+    data(){
+      return {
+        inputvalue:''
+      }
+    },
+    components:{
+      'Search_input':SearchBar
+    },
+    methods:{
+      submit(){
+        console.log(this.inputvalue)
+      }
+    }
 }
 </script>
 
