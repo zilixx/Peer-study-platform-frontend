cb373169e3953a8c52bd23443993b2190b93c506
Nio Wang <swan4287@uni.sydney.edu.au>
2020-10-21 11:27:12 +1100

    edited searchbar component, moved the button to Searchbar

--- src/components/content/AllCourse.vue
+++ src/components/content/AllCourse.vue
@@ -1,11 +1,8 @@
 <template>
   <div>
     <h1>This is course overview content</h1>
-    <div style="float: left">
+    <div>
       <Search_input v-model="inputvalue"></Search_input>
-    </div>
-    <div style="float: right">
-      <button class="button button1" @click="submit">Search</button>
     </div>
   </div>
 
@@ -32,25 +29,5 @@
 </script>
 
 <style>
-  .button {
-    width: 100%;
-    background-color: #4CAF50;
-    color: white;
-    padding: 8px 20px;
-    /*margin: 8px 0;*/
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-  }
 
-  .button1{
-    background-color: #4CAF50;
-    color: white;
-    border: none;
-  }
-
-  .button1:hover {
-    background-color: white;
-    color: #4CAF50;
-  }
 </style>
--- src/components/utils/SearchBar.vue
+++ src/components/utils/SearchBar.vue
@@ -1,8 +1,13 @@
 <!-- Search bar component -->
 <template xmlns:size="http://www.w3.org/1999/xhtml">
-    <div>
+  <div>
+    <div style="float: left">
         <input :value="currentValue" @input="inputload" placeholder="Please enter class number, eg: COMP5619" class="input" size="165.5">
     </div>
+    <div style="float: right">
+        <button class="button button1" @click="submit">Search</button>
+    </div>
+  </div>
 </template>
 <script>
     export default {
@@ -40,5 +45,26 @@
 <style>
     .input{
         padding: 6px 10px;
+    }
+    .button {
+      width: 100%;
+      background-color: #4CAF50;
+      color: white;
+      padding: 8px 20px;
+      /*margin: 8px 0;*/
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+
+    .button1{
+      background-color: #4CAF50;
+      color: white;
+      border: none;
+    }
+
+    .button1:hover {
+      background-color: white;
+      color: #4CAF50;
     }
 </style>
