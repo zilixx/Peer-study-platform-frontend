e987eb7fedd85114c58a45f635d795c0348fcdc3
Limou Zhou <lzho9304@uni.sydney.edu.au>
2020-10-27 20:58:13 +1100

    Login Supporter

--- src/main.js
+++ src/main.js
@@ -9,11 +9,30 @@ import api from './utils/api.js'
 import './plugins/element.js'
 import './theme/index.css'
 
+//Import Store
+import store from './store/index.js'
+
 Vue.prototype.$axios = api;
 
 Vue.config.productionTip = false;
 
+router.beforeEach((to,from,next) => {
+    // let isLogin = sessionStorage.getItem('isLogin')
+
+    if (to.path == '/logout'){
+        sessionStorage.clear();
+        next('/login');
+    }else if (to.path == '/login'){
+        // if(isLogin != null){
+        //     next('/main');
+        // }
+        window.location.href = 'http://localhost:8888';
+        next(false);
+    }
+})
+
 new Vue({
+    store,
     render: h => h(App),
     router
 }).$mount('#app')
