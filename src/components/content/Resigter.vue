55b149424f73afc26cf1db58d7f54f4129a2ee39
Qihang Zhao <qzha5529@uni.sydney.edu.au>
2020-10-28 13:36:53 +1100

    modify style

--- src/components/content/Resigter.vue
+++ src/components/content/Resigter.vue
@@ -1,46 +1,50 @@
 <template>
-  <div>
-    <h3>Select Courses</h3>
-    <el-select v-model="course" placeholder="please choose" id="selection" class="selection" action="http://localhost:8888/register"
-               @change="submitCourse()">
-      <el-option
-        v-for="item in options"
-        :key="item.name"
-        :value="item.courseId"
-        :label="item.name"
-        >
-      </el-option>
-    </el-select>
+    <div>
+        <h3>Select Courses</h3>
+        <el-select v-model="course" placeholder="please choose" id="selection" class="selection"
+                   action="http://localhost:8888/register"
+                   @change="submitCourse()">
+            <el-option
+                v-for="item in options"
+                :key="item.name"
+                :value="item.courseId"
+                :label="item.name"
+            >
+            </el-option>
+        </el-select>
 
-    <h3>Select Available Time</h3>
-    <el-select v-model="availableDay" placeholder="please choose" id="selection" class="selection"
-               @change="submitDay()">
-      <el-option
-          v-for="item in day"
-          :key="item.value"
-          :value="item.label"
-      >
-      </el-option>
-    </el-select>
+        <h3>Select Available Time</h3>
+        <el-select v-model="availableDay" placeholder="please choose" id="selection" class="selection"
+                   @change="submitDay()">
+            <el-option
+                v-for="item in day"
+                :key="item.value"
+                :value="item.label"
+            >
+            </el-option>
+        </el-select>
 
-    <h3>Upload CV</h3>
-    <el-upload
-        class="upload"
-        ref="upload"
-        action="http://localhost:8888/register/upload"
-        :on-preview="handlePreview"
-        :on-remove="handleRemove"
-        :before-remove="beforeRemove"
-        :limit="1"
-        :file-list="fileList"
-        :auto-upload="false">
-      <el-button slot="trigger" size="small" type="primary">Select a file</el-button>
-      <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">Confirm</el-button>
-      <div slot="tip" class="el-upload__tip">No larger than 10MB</div>
-    </el-upload>
-<!--    <uploading></uploading>-->
+        <h3>Upload CV</h3>
+        <el-upload
+            class="upload"
+            ref="upload"
+            action="http://localhost:8888/register/upload"
+            :on-preview="handlePreview"
+            :on-remove="handleRemove"
+            :before-remove="beforeRemove"
+            :limit="1"
+            :file-list="fileList"
+            :auto-upload="false">
+            <el-button slot="trigger" size="small" type="primary">Select a file</el-button>
 
-  </div>
+            <div slot="tip" class="el-upload__tip">No larger than 10MB</div>
+        </el-upload>
+        <br>
+        <el-button style="margin-left: 10px;" type="success" @click="submitUpload" id="confirm-register">Confirm<i
+            class="el-icon-upload el-icon--right"></i></el-button>
+        <!--    <uploading></uploading>-->
+
+    </div>
 
 </template>
 
@@ -48,106 +52,105 @@
 // import uploading from "../utils/Upload"
 
 export default {
-  name: "Register",
-  // components: {
-  //   uploading
-  // },
-  data() {
-    return {
-      options: [],
-      day: [
-        {
-          value: '1',
-          label: 'Monday'
-        }, {
-          value: '2',
-          label: 'Tuesday'
-        }, {
-          value: '3',
-          label: 'Wednesday'
-        }, {
-          value: '4',
-          label: 'Thursday'
-        }, {
-          value: '5',
-          label: 'Friday'
-        }, {
-          value: '6',
-          label: 'Saturday'
-        }, {
-          value: '7',
-          label: 'Sunday'
+    name: "Register",
+    // components: {
+    //   uploading
+    // },
+    data() {
+        return {
+            options: [],
+            day: [
+                {
+                    value: '1',
+                    label: 'Monday'
+                }, {
+                    value: '2',
+                    label: 'Tuesday'
+                }, {
+                    value: '3',
+                    label: 'Wednesday'
+                }, {
+                    value: '4',
+                    label: 'Thursday'
+                }, {
+                    value: '5',
+                    label: 'Friday'
+                }, {
+                    value: '6',
+                    label: 'Saturday'
+                }, {
+                    value: '7',
+                    label: 'Sunday'
+                }
+            ],
+            course: '',
+            availableDay: '',
+            fileList: [],
+        };
+    },
+    mounted() {
+        this.$axios.get("http://localhost:8888/register/all", {
+            params: {course: this.course}
+        })
+            .then((res) => {
+                this.options = res.data
+                this.$message({
+                    message: "Data updated.",
+                    type: "info",
+                    duration: 1000
+                })
+            }).catch((err) => {
+            console.log(err)
+        })
+    },
+    methods: {
+        submitUpload() {
+            this.$refs.upload.submit();
+        },
+        handleRemove(file, fileList) {
+            console.log(file, fileList);
+        },
+        handlePreview(file) {
+            console.log(file);
+        },
+        beforeRemove(file) {
+            return this.$confirm(`Are you sure to delete ${file.name}？`);
+        },
+        submitCourse() {
+            console.log('You chose', this.course)
+            this.$axios.get("http://localhost:8888/register/select", {
+                params: {courseId: this.course}
+            }).then((res) => {
+                console.log(res.data)
+            })
+
+        },
+        submitDay() {
+            console.log('You chose', this.availableDay)
+            this.$axios.get("http://localhost:8888/register/selectDay", {
+                params: {day: this.availableDay}
+            }).then((res) => {
+                console.log(res.data)
+            })
         }
-      ],
-      course: '',
-      availableDay: '',
-      fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}],
-    };
-  },
-  mounted() {
-    this.$axios.get("http://localhost:8888/register/all", {
-      params: {course: this.course}
-    })
-        .then((res)=>{
-          this.options = res.data
-          this.$message({
-            message: "Data updated.",
-            type: "info",
-            duration: 1000
-          })
-        }).catch((err) => {console.log(err)})
-  },
-  methods: {
-    submitUpload() {
-      this.$refs.upload.submit();
-    },
-    handleRemove(file, fileList) {
-      console.log(file, fileList);
-    },
-    handlePreview(file) {
-      console.log(file);
-    },
-    beforeRemove(file) {
-      return this.$confirm(`Are you sure to delete ${ file.name }？`);
-    },
-    submitCourse() {
-      console.log('您选择了', this.course)
-      this.$axios.get("http://localhost:8888/register/select", {
-        params: {courseId: this.course}
-      }).then((res)=>{
-        console.log(res.data)
-      })
-      //     .then((res) => {
-      //   if (res.data.response.deleteStat){
-      //     // 抄来的管用代码，link: https://segmentfault.com/q/1010000011726114
-      //     let deletedItem = this.resList.splice(
-      //         this.resList.findIndex(item => item.id === itemId), 1)
-      //     console.log(deletedItem)
-      //     this.deleteHint(true) // TODO: possible async ops here
-      //   } else {
-      //     this.deleteHint(false)
-      //   }
-      // })
-    },
-    submitDay() {
-      console.log('您选择了', this.availableDay)
-      this.$axios.get("http://localhost:8888/register/selectDay", {
-        params: {day: this.availableDay}
-      }).then((res)=>{
-        console.log(res.data)
-      })}
-  }
+    }
 
 }
 </script>
 
 <style>
-.selection{
-  width: 400px;
-  margin: 20px auto;
+.selection {
+    width: 400px;
+    margin: 20px auto;
 }
+
 .upload {
-  width: 400px;
-  margin: 20px auto;
+    width: 400px;
+    margin: 20px auto;
 }
+
+#confirm-register{
+    width: 400px;
+}
+
 </style>
