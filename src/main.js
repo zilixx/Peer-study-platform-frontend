c14b4dbff792be3a3fef9a57f90c4adb991c1c13
Limou Zhou <lzho9304@uni.sydney.edu.au>
2020-10-29 15:58:28 +1100

    Login Guards Completed

--- src/main.js
+++ src/main.js
@@ -17,19 +17,52 @@ Vue.prototype.$axios = api;
 Vue.config.productionTip = false;
 
 router.beforeEach((to,from,next) => {
-    // let isLogin = sessionStorage.getItem('isLogin')
+    let cookies = document.cookie.split(' ');
+
+    if (cookies.length > 1) {
+        for (var i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i].split('=')
+            let key = cookie[0];
+            let value = cookie[1].slice();
+            sessionStorage.setItem(key, value);
+        }
+    }
+    let isLogin = sessionStorage.getItem("isLogin");
 
     if (to.path == '/logout'){
+        deleteAllCookies();
         sessionStorage.clear();
+        isLogin = null;
         next('/login');
-    }else if (to.path == '/login'){
-        // if(isLogin != null){
-        //     next('/main');
-        // }
-        window.location.href = 'http://localhost:8888';
-        next(false);
+    }
+    else if (to.path == '/login'){
+        if(isLogin != null){
+            next('/');
+        }else{
+            window.location.href = 'http://localhost:8888';
+            next(false);
+        }
+    }
+    else{
+        if (isLogin != null) {
+            next();
+        }else {
+            window.location.href = 'http://localhost:8888';
+            next(false);
+        }
     }
 })
+
+function deleteAllCookies() {
+    let cookies = document.cookie.split(";");
+
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i];
+        let eqPos = cookie.indexOf("=");
+        let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
 
 new Vue({
     store,
